1e7fe4b84a6087ea9a83673dfcada2eefebb09f0
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-01-18 11:01:24 +0000

    initial upload

---
+++ CcAutoWash.bat
@@ -0,0 +1,28 @@
+@echo off
+
+:: Initialization
+title CcAutoWash
+mode 30,11
+color 80
+cd /d "%~dp0"
+set "scriptDir=%~dp0"
+echo.
+echo Script Initialized...
+echo.
+echo.
+
+:: Introduction
+echo CcAutoWash In 10 Seconds..
+timeout /t 10 /nobreak >nul
+
+:: Start Cleaning
+echo ..Executing CCleaner..
+"%scriptDir%CCleaner64.exe" /AUTO
+echo ..Cleaning Complete.
+echo.
+
+:: Script Exit
+echo.
+echo ...Script Complete.
+timeout /t 5 /nobreak >nul
+exit
